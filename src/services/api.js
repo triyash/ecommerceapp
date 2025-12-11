357fd17e0eb307b5827ec1dfea66577735d402f8
triyash <yashtrivedi1107@gmail.com>
2025-12-11 23:33:13 +0530

    Create api.js

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+const API_BASE = "https://fakestoreapi.com";
+
+export async function fetchProducts() {
+  const res = await axios.get(`${API_BASE}/products`);
+  return res.data;
+}
+
+export async function fetchProduct(id) {
+  const res = await axios.get(`${API_BASE}/products/${id}`);
+  return res.data;
+}
